ee19df19c41e575cc045569ece7d90987c7bed3c
Liesel Downes <cocoiadrop@users.noreply.github.com>
2021-01-24 13:36:40 +0000

    Apply fixes from StyleCI

--- routes/web-site.php
+++ routes/web-site.php
@@ -8,7 +8,5 @@ use App\Http\Controllers\Site\ViewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('site.')->group(function () {
-
-Route::get('/', [ViewsController::class, 'welcome'])->name('welcome');
-
+    Route::get('/', [ViewsController::class, 'welcome'])->name('welcome');
 });
